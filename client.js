d8de63eee0cab592319051b6170c12080773760d
Will <wdportman@gmail.com>
2020-09-23 22:59:45 +0000

    created constants file

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 const net = require('net');
 
+const { IP, PORT } = require('./constants');
+
 const connect = function() {
   const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
+    host: IP,
+    port: PORT
   });
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
