2d499787aa741f56a1431665578108d0e16d1725
Will <wdportman@gmail.com>
2020-09-23 20:29:23 +0000

    separated connect function into separate file

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.222.131",
+    port: 50542
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log(`${data}`)
+  });
+  return conn;
+}
+
+module.exports = { connect }
--- play.js
+++ play.js
@@ -1,16 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.222.131",
-    port: 50542
-  });
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log(`${data}`)
-  });
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
